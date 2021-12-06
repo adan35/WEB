1f7f3b3ea5f737c0310b03b17be9c8a290221449
adan35 <adanshahzad35@gmail.com>
2021-12-06 21:56:11 +0500

    adding cart-drop-down to app

--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+import { connect } from "react-redux";
+import toggleCardHidden from "../../redux/cart/cart.actions";
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
-const CardIcon = () => (
-    <div className="cart-icon">
+const CardIcon = ({ toggleCardHidden }) => (
+    <div className="cart-icon" onClick={toggleCardHidden}>
         <ShoppingIcon className="shopping-icon" />
         <span className="item-count">0</span>
     </div>
 )
 
-export default CardIcon;
+const mapDispatchToProps = (dispatch) => ({
+    toggleCardHidden: () => dispatch(toggleCardHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CardIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,9 @@ import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils"
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CardIcon from "../cart-icon/cart-icon.components";
+import CardDropDown from "../cart-dropdown/cart-dropdown.components";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className="header">
             <Link className="logo-container" to="/">
@@ -23,12 +24,17 @@ const Header = ({ currentUser }) => {
                 }
                 <CardIcon />
             </div>
+            {
+                hidden ? null :
+                <CardDropDown />
+            }
         </div>
     )
 }
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
