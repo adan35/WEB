ab8c0fa5694af1678e7674f2be253a99896de370
adan35 <adanshahzad35@gmail.com>
2021-12-06 20:56:50 +0500

    adding cart-icon to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,22 +4,24 @@ import "./header.styles.scss"
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils"
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CardIcon from "../cart-icon/cart-icon.components";
 
-const Header = ({currentUser}) => {
+const Header = ({ currentUser }) => {
     return (
         <div className="header">
             <Link className="logo-container" to="/">
-                <Logo className="logo"/>
+                <Logo className="logo" />
             </Link>
             <div className="options">
                 <Link className="option" to="/shop">SHOP</Link>
                 <Link className="option" to="/shop">CONTACT</Link>
                 {
                     currentUser ?
-                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-                    :
-                    <Link className="option" to="/signin">SIGN IN</Link>
+                        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                        :
+                        <Link className="option" to="/signin">SIGN IN</Link>
                 }
+                <CardIcon />
             </div>
         </div>
     )
