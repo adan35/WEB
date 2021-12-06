8b4cec5886b89168c5ceea17d7022221e875bec9
adan35 <adanshahzad35@gmail.com>
2021-12-06 22:41:04 +0500

    adding hover effect on collection-item

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./custom-button.styles.scss"
 
-export const CustomButton = ({children, isGoogleSignin, ...otherProps}) => {
+export const CustomButton = ({children, isGoogleSignin, inverted, ...otherProps}) => {
     return (
-        <div className={`${isGoogleSignin ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+        <div className={`${inverted ? 'inverted' : ''} ${isGoogleSignin ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
             {children}
         </div>
     )
