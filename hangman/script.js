44c90ae0ed931581e615b7978cf42352ec6b1155
adan35 <adanshahzad35@gmail.com>
2022-06-14 11:41:27 +0500

    displayWord function in js

---
+++ hangman/script.js
@@ -0,0 +1,37 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['apple', 'banana', 'grapes', 'mango', 'orange', 'pineapple', 'strawberry'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = ['a', 'p', 'l', 'e'];
+const wrongLetters = [];
+
+// Show the hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(letter => `
+        <span class="letter">
+          ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `)
+        .join('')}
+  `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+  if(innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won! 😃';
+    popup.style.display = 'flex';
+  }
+}
+
+displayWord();
